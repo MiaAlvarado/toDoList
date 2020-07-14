840e67b92a9bc44a3f8c1e90a75641c7b3ede9d5
Mia Alvarado <mealvarado17224@gmail.com>
2020-07-14 13:44:04 -0700

    Connect to GitHub

---
+++ toDoList/AddToDoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddToDoViewController.swift
+//  toDoList
+//
+//  Created by Mia Elizabeth Alvarado on 7/14/20.
+//  Copyright © 2020 Mia Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ toDoList/ToDo.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Mia Elizabeth Alvarado on 7/14/20.
+//  Copyright © 2020 Mia Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+  var name = ""
+    var important = false
+    
+    
+}
